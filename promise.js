4469f1d9ddee06e0ffefcd98f41cff28a5bc5271
vivian5668 <chelsea.sophon@gmail.com>
2019-01-10 10:34:59 -0800

    add exmaple for async, await, fetch

--- promise.js
+++ promise.js
@@ -28,19 +28,42 @@ function createPosts(post) {
     });
 }
 
-//one promise
-createPosts({ title: 'Post Three', body: 'THis is post 3' })
-    .then(getPosts)
-    .catch(err => console.log(err));
+// //one promise
+// createPosts({ title: 'Post Three', body: 'THis is post 3' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
 
-//Promise.all
-const promise1 = Promise.resolve('Hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000, 'Goodbye')
-});
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+// //Promise.all
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'Goodbye')
+// });
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-Promise.all([promise1, promise2, promise3, promise4]).then((
-    (values) => console.log(values)
-))
+// Promise.all([promise1, promise2, promise3, promise4]).then((
+//     (values) => console.log(values)
+// ))
+
+
+//async
+// if you want to use await, you have to say 'async function' when initiating the function
+async function makePosts() {
+    await createPosts({ title: 'Post Three', body: 'THis is post 3' });
+    getPosts();
+}
+
+makePosts();
+
+//async / await/ fetch
+
+async function fetchUsers() {
+    const res = await fetch
+    ('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data)
+}
+
+fetchUsers();
